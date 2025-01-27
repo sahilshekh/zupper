7b312ac83f339f04e86faa1181177cab7372c5ca
Sahil Shekh <95908470+sahilshekh@users.noreply.github.com>
2025-01-27 22:17:12 +0530

    not go with dummy data this time go with prod data

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,12 +1,26 @@
 import RestroCard from "../RestroCard/RestroCard";
 import ListObj from "../../utils/mockData";
 
-import {useState} from "react"
+import {useState,useEffect} from "react"
 
 const Body = () => {
 
-  const [rated,useRated] =useState(ListObj)
-   
+  const [rated,useRated] =useState([])
+// console.log(rated,"rated")
+   useEffect(()=>{
+     fetchData()
+   },[])
+
+   const fetchData = async ()=>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0263166&lng=80.2063355&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+      const result =await data.json() 
+
+     
+          useRated(result.data.cards[4]["card"]["card"]["gridElements"]["infoWithStyle"]["restaurants"])
+          
+       
+      
+  }
   return (
     <div className="body">
       <button  className="btn"  onClick={()=>{
